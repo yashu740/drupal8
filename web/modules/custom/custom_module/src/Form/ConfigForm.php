3ee4030f19563216a5e4da062e6d49fb703a2707
yashu <yashu.b06@gmail.com>
2020-07-06 18:55:42 +0530

    Config form creation

---
+++ web/modules/custom/custom_module/src/Form/ConfigForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\custom_module\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class ConfigForm.
+ *
+ * @package Drupal\custom_module\Form
+ */
+class ConfigForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'custom_module.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'config_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+    $config = $this->config('custom_module.settings');
+    $form['body'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('body'),
+      '#default_value' => $config->get('body'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('custom_module.settings')
+      ->set('body', $form_state->getValue('body'))
+      ->save();
+  }
+
+}
