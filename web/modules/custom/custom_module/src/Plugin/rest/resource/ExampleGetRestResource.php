d919dfae46eee67351828643ba7bd01dc2d70e1e
yashu <yashu.b06@gmail.com>
2020-08-27 18:35:36 +0530

    Validation for the date format

--- web/modules/custom/custom_module/src/Plugin/rest/resource/ExampleGetRestResource.php
+++ web/modules/custom/custom_module/src/Plugin/rest/resource/ExampleGetRestResource.php
@@ -80,41 +80,77 @@ class ExampleGetRestResource extends ResourceBase {
    * @throws \Symfony\Component\HttpKernel\Exception\HttpException
    *   Throws exception expected.
    */
-  public function get($timestamp = NULL) {
+  public function get($timestamp = '') {
 
-    if ($timestamp) {
-      $nids = \Drupal::entityQuery('node')->condition('status', 1)->condition('created', $timestamp, '>')->execute();
-      $nodes = Node::loadMultiple($nids);
+    $time = explode("=", $timestamp);
 
-      $data = [];
+    if (!empty($time[1] && is_numeric($time[1]))) {
 
-      foreach ($nodes as $node) {
-        $pdf_fid = ($node->get('field_pdf')->isEmpty() ? 0 : $node->get('field_pdf')->getValue()[0]['target_id']);
-        $image_fid = ($node->get('field_images')->isEmpty() ? 0 : $node->get('field_images')->getValue()[0]['target_id']);
-        $data[] = [
-          'pdf_fid' => $pdf_fid,
-          'image_fid' => $image_fid,
+      $human_date = date('m-d-Y', $time[1]);
+      $data_split = explode("-", $human_date);
+      $month = $data_split[0];
+      $day = $data_split[1];
+      $year = $data_split[2];
 
-          'type' => $node->get('type')->target_id,
-          'id' => $node->get('nid')->value,
+      // Query to fetch the nids
+      // validate the  timestamp is proper.
+      if (checkdate($month, $day, $year)) {
 
-          'title' => $node->get('title')->value,
-          'body' => $node->get('body')->value,
-          'boolean' => $node->get('field_boolean')->value,
-          'number' => $node->get('field_contact')->value,
-          'selection' => $node->get('field_select')->value,
-          'Description' => $node->get('field_text')->value,
-          'url' => $node->get('field_url')->uri,
-        /*'ee' => $node->getCreatedTime(),*/
+        $query = \Drupal::database()->select('node_field_data', 'nf');
+        $query->fields('nf', ['nid']);
+        $query->condition('nf.type', 'example_rest_api');
+        $query->condition('nf.created', $time[1], '>=');
+        $nids = $query->execute()->fetchAll();
+        $node_nid = [];
+        foreach ($nids as $key => $nid) {
+          $node_nid[$nid->nid] = $nid->nid;
+        }
+        $node_data = $node_nid;
 
-        ];
-
-        $data = $data + [
-          'pdf_uri' => $this->restImageUri($pdf_fid),
-          'image_uri' => $this->restImageUri($image_fid),
-        ];
       }
+      else {
+        $data = "The entered date format is wrong.";
+        $response = new ResourceResponse($data);
+        $response->addCacheableDependency($data);
+        return $response;
+      }
+
     }
+    else {
+
+      $nids = \Drupal::entityQuery('node')->condition('status', 1)->condition('type', 'example_rest_api')->execute();
+      $node_data = $nids;
+    }
+
+    $nodes = Node::loadMultiple($node_data);
+    $data = [];
+
+    foreach ($nodes as $node) {
+      $pdf_fid = ($node->get('field_pdf')->isEmpty() ? 0 : $node->get('field_pdf')->getValue()[0]['target_id']);
+      $image_fid = ($node->get('field_images')->isEmpty() ? 0 : $node->get('field_images')->getValue()[0]['target_id']);
+      $data[] = [
+        'pdf_fid' => $pdf_fid,
+        'image_fid' => $image_fid,
+
+        'type' => $node->get('type')->target_id,
+        'id' => $node->get('nid')->value,
+
+        'title' => $node->get('title')->value,
+        'body' => $node->get('body')->value,
+        'boolean' => $node->get('field_boolean')->value,
+        'number' => $node->get('field_contact')->value,
+        'selection' => $node->get('field_select')->value,
+        'Description' => $node->get('field_text')->value,
+        'url' => $node->get('field_url')->uri,
+
+      ];
+
+      $data = $data + [
+        'pdf_uri' => $this->restImageUri($pdf_fid),
+        'image_uri' => $this->restImageUri($image_fid),
+      ];
+    }
+
     $response = new ResourceResponse($data);
     $response->addCacheableDependency($data);
     return $response;
@@ -130,6 +166,19 @@ class ExampleGetRestResource extends ResourceBase {
       return $file_uri;
     }
 
+  }
+
+  /**
+   * Conversion of date format.
+   */
+  public function changeDateFormat($date_string, $format_string = 'j-F-Y H:i:s') {
+    print_r($date_string);
+    if (!$this->isTimestamp($date_string)) {
+
+      $date_string = strtotime($date_string);
+    }
+
+    return date($format_string, $date_string);
   }
 
 }
